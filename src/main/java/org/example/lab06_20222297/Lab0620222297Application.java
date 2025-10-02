457c0dc35da3b0286b558e5c55a2c1eea6688b87
Fabricio Tirado <a20222297@pucp.edu.pe>
2025-10-02 18:45:22 -0500

    segundo commit

---
+++ src/main/java/org/example/lab06_20222297/Lab0620222297Application.java
@@ -0,0 +1,13 @@
+package org.example.lab06_20222297;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0620222297Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab0620222297Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lab06_20222297/Lab0620222297ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab06_20222297;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0620222297ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
